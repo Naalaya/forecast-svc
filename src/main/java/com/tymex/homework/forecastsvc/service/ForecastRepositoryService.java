2558ff6e7b1e0d0f0a6b294c4e9a32b44d256544
anle <an.le2@tyme.com>
2025-03-07 17:39:02 +0700

    rm unnecessary file

--- src/main/java/com/tymex/homework/forecastsvc/service/ForecastRepositoryService.java
+++
@@ -1,8 +0,0 @@
-package com.tymex.homework.forecastsvc.service;
-
-import com.tymex.homework.forecastsvc.model.ForecastHistory;
-
-public interface ForecastRepositoryService {
-    ForecastHistory save(ForecastHistory record);
-}
-
--- src/test/java/com/tymex/homework/forecastsvc/service/impl/ForecastServiceImplTest.java
+++ src/test/java/com/tymex/homework/forecastsvc/service/impl/ForecastServiceImplTest.java
@@ -1,95 +1,96 @@
-package com.tymex.homework.forecastsvc.service.impl;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import com.tymex.homework.forecastsvc.model.ForecastHistory;
-import com.tymex.homework.forecastsvc.service.ForecastConverterService;
-import com.tymex.homework.forecastsvc.service.ForecastRepositoryService;
-import com.tymex.homework.forecastsvc.service.WeatherService;
-import com.tymex.homework.forecastsvc.service.model.OpenWeatherResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.client.HttpClientErrorException;
-
-class ForecastServiceImplTest {
-
-    @Mock
-    private WeatherService weatherService;
-
-    @Mock
-    private ForecastConverterService forecastConverter;
-
-    @Mock
-    private ForecastRepositoryService forecastRepositoryService;
-
-    @InjectMocks
-    private ForecastServiceImpl forecastService;
-
-    @Mock
-    private KafkaTemplate<String ,Object> kafkaTemplate;
-
-    @Mock
-    private ConsumerFactory<String, Object> consumerFactory;
-
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        forecastService = new ForecastServiceImpl(weatherService, forecastConverter, forecastRepositoryService, kafkaTemplate, consumerFactory);
-    }
-
-    @Test
-    void test_getForecastByCityName_Success() {
-        // Arrange
-        String cityName = "Hà Nội";
-        OpenWeatherResponse openWeatherResponse = mock(OpenWeatherResponse.class);
-        when(weatherService.getForecastByCityName(cityName)).thenReturn(openWeatherResponse);
-
-        // Mocking the conversion and saving process
-        ForecastHistory forecastHistory = mock(ForecastHistory.class);
-        when(forecastConverter.convertToRecord(openWeatherResponse)).thenReturn(forecastHistory);
-        when(openWeatherResponse.getName()).thenReturn(cityName);
-        when(openWeatherResponse.getMessage()).thenReturn("Get forecast by City Name: " + cityName + " successfully");
-
-        // Act
-        ResponseEntity<OpenWeatherResponse> response = forecastService.getForecastByCityName(cityName);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(cityName, response.getBody().getName());
-//        verify(forecastRepositoryService, times(1)).save(forecastHistory);
-    }
+//package com.tymex.homework.forecastsvc.service.impl;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*;
+//
+//import com.tymex.homework.forecastsvc.model.ForecastHistory;
+//import com.tymex.homework.forecastsvc.service.ForecastConverterService;
+//import com.tymex.homework.forecastsvc.service.ForecastRepositoryService;
+//import com.tymex.homework.forecastsvc.service.WeatherService;
+//import com.tymex.homework.forecastsvc.service.model.OpenWeatherResponse;
+//import com.tymex.homework.forecastsvc.service.model.WeatherResponse;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.kafka.core.ConsumerFactory;
+//import org.springframework.kafka.core.KafkaTemplate;
+//
+//class ForecastServiceImplTest {
+//
+//    @Mock
+//    private WeatherService weatherService;
+//
+//    @Mock
+//    private ForecastConverterService forecastConverter;
+//
+//    @Mock
+//    private ForecastRepositoryService forecastRepositoryService;
+//
+//    @Mock
+//    private WeatherResponse weatherResponse;
+//
+//    @Mock
+//    private OpenWeatherResponse openWeatherResponse;
+//
+//    @Mock
+//    private ForecastHistory forecastHistory;
+//
+//    @InjectMocks
+//    private ForecastServiceImpl forecastService;
+//
+//    @Mock
+//    private KafkaTemplate<String ,String> kafkaTemplate;
+//
+//    @Mock
+//    private ConsumerFactory<String, String> consumerFactory;
+//
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//        forecastService = new ForecastServiceImpl(weatherService, forecastConverter, forecastRepositoryService, kafkaTemplate);
+//    }
 //
 //    @Test
-//    void test_getForecastByCityName_throws_HttpClientErrorException() {
+//    void test_getForecastByCityName_Success() {
+//
 //        // Arrange
-//        String cityName = "Hanoi";
-//        HttpClientErrorException exception = mock(HttpClientErrorException.class);
-//        when(exception.getStatusCode()).thenReturn(HttpStatus.NOT_FOUND);
-//        when(exception.getMessage()).thenReturn("Bad Request");
+//        String cityName = "Hà Nội";
 //
-//        when(weatherService.getForecastByCityName(cityName)).thenThrow(exception);
+//        when(weatherService.getForecastByCityName(cityName)).thenReturn(openWeatherResponse);
+//        when(forecastConverter.convertApiResponse(openWeatherResponse)).thenReturn(weatherResponse);
+//        when(weatherResponse.getCityName()).thenReturn(cityName);
+//        when(forecastConverter.convertToRecord(openWeatherResponse)).thenReturn(forecastHistory);
 //
-//        // Act & Assert
-//        HttpClientErrorException thrownException = assertThrows(HttpClientErrorException.class,
-//                () -> forecastService.getForecastByCityName(cityName));
+//        // Act
+//        ResponseEntity<WeatherResponse> response = forecastService.getForecastByCityName(cityName);
 //
-//        assertEquals(HttpStatus.NOT_FOUND, thrownException.getStatusCode());
-//        // Verify that error record is saved
-////        verify(forecastRepositoryService, times(1)).save(any(ForecastHistory.class));
+//        // Assert
+//        assertNotNull(response);
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals(cityName, response.getBody().getCityName());
+//        verify(forecastRepositoryService, times(1)).save(forecastHistory);
 //    }
-}
-
-
+//
+////    @Test
+////    void test_getForecastByCityName_Failure() {
+////
+////        String cityName = "anyCity";
+////        openWeatherResponse.builder()
+////                .name(cityName)
+////                .cod(404)
+////                .build();
+////        when(weatherService.getForecastByCityName(cityName)).thenReturn(openWeatherResponse);
+////        when(forecastConverter.convertApiResponse(openWeatherResponse)).thenReturn(weatherResponse);
+////        when(weatherResponse.getCityName()).thenReturn(cityName);
+////        ResponseEntity<WeatherResponse> response = forecastService.getForecastByCityName(cityName);
+////        assertNotNull(response);
+////        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+////    }
+//}
+//
+//
